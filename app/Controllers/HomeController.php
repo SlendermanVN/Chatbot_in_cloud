08acdb47fa4f495a396e8b8bbc20d6d88b6820b3
SlendermanVN <nguyenleanhduc2004@gmail.com>
2026-06-06 23:28:47 +0700

    fix direction of file HomeController.php

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/../app/Models/Product.php';
-require_once __DIR__ . '/../app/Models/News.php';
+require_once __DIR__ . '/../Models/Product.php';
+require_once __DIR__ . '/../Models/News.php';
 
 require_once __DIR__ . '/BaseController.php';
 
